5875546c3834819bd88a3a7d534c16d934980239
CharaD7 <jijakahn6@gmail.com>
2024-02-18 17:49:10 +0000

    Imported fetchWrapper, implemented async await and enforced error instance check

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,13 +1,20 @@
 import { GraphQLClient } from "@refinedev/nestjs-query";
+import { fetchWrapper } from "./fetch-wrapper";
 
 export const API_URL = "https://api.crm.refine.dev";
 
 export const client = new GraphQLClient(API_URL, {
-  fetch: (url: string, options: RequestInit) => {
+  fetch: async (url: string, options: RequestInit) => {
     try {
-      return fetchWrapper(url, options);
+      return await fetchWrapper(url, options);
     } catch (error) {
-      return Promise.reject(error as Error);
+      // Ensure the rejection reason is an Error
+      if (error instanceof Error) {
+        return await Promise.reject(error);
+      } else {
+        // If it's not, create a new Error with the given error as the message
+        return await Promise.reject(new Error(String(error)));
+      }
     }
   }
 });

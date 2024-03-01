7e9e07aa7c56456e41bd9c21e214bc1c2423c26a
CharaD7 <jijakahn6@gmail.com>
2024-03-01 23:55:38 +0000

    Declare authCredentials constant

---
+++ src/providers/auth.ts
@@ -0,0 +1,7 @@
+import { AuthBindings } from "@refinedev/core";
+import { API_URL, dataProvider } from "./data";
+
+export const authCredentials = {
+  email: "micael.scott@dundermifflin.com",
+  password: "demodemo",
+};

89caede9499aa9238df555e2403d6db3ce3ec845
CharaD7 <jijakahn6@gmail.com>
2024-03-02 00:00:07 +0000

    Test login mutation

--- src/providers/auth.ts
+++ src/providers/auth.ts
@@ -5,3 +5,22 @@ export const authCredentials = {
   email: "micael.scott@dundermifflin.com",
   password: "demodemo",
 };
+
+export const authProvider: AuthBindings = {
+  login: async ({ email }) => {
+    try {
+      // call the login mutation
+      // dataProvider.custom is used to make a custom request to the GraphQL API
+      // this will call dataProvider which will go through the fetchWrapper function
+      const { data } = await dataProvider.custom({
+        url: API_URL,
+        method: "post",
+        headers: {},
+        meta: {
+          variables: { email },
+          // pass the email to see if th euser exists and if so, return the accessToken
+        }
+      });
+    }
+  },
+};
